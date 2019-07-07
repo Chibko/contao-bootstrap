169deabae7ec40f1dc843e738b1c1f33655c8bec
Stéphane Chibko <info@chibko.com>
2019-07-07 14:20:32 +0200

    Test

--- classes/Hooks.php
+++ classes/Hooks.php
@@ -7,7 +7,7 @@ class Hooks {
 	public function myGeneratePage($objPage, $objLayout, $objPageRegular) {
 		if ($objLayout->bootstrap) {
 				$GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/xBootstrap/assets/js/bootstrap.min.js|static';
-				$GLOBALS['TL_CSS'][] = 'system/modules/xBootstrap/assets/css/bootstrap.min.css|static';
+				$GLOBALS['TL_CSS'][] = 'system/modules/xBootstrap/assets/css/bootstrap.min.css';
 		}
 		$strDefersTags = '[[TL_DEFERS]]';
 		if (($strDefers = trim($objLayout->javascript)) != false) {
